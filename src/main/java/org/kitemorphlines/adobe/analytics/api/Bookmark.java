d4ffa4008869110c16c1a91a15fce519f856f30e
Leonardo Foderaro <startrack79@gmail.com>
2016-03-04 10:11:18 +0100

    first supported API! Bookmark.GetBookmarks

--- src/main/java/org/kitemorphlines/adobe/analytics/api/Bookmark.java
+++ src/main/java/org/kitemorphlines/adobe/analytics/api/Bookmark.java
@@ -1,22 +1,53 @@
 package org.kitemorphlines.adobe.analytics.api;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.kitemorphlines.adobe.analytics.annotations.AdobeAnalyticsAPI;
 import org.kitemorphlines.adobe.analytics.annotations.AdobeAnalyticsAPIMethod;
+import org.kitemorphlines.adobe.analytics.commands.GenericCommand;
+import org.kitesdk.morphline.api.Record;
+import org.kitesdk.morphline.base.Fields;
 
-import com.adobe.analytics.client.AnalyticsClient;
-
+import com.adobe.analytics.client.domain.BookmarkFolder;
 
 @AdobeAnalyticsAPI(name="Bookmark")
 public class Bookmark extends AdobeAnalyticsBaseAPI {
 	
-	private AnalyticsClient client;
-
 	@AdobeAnalyticsAPIMethod(name="GetBookmarks")
-	public void getBookmarks() {
+	public boolean getBookmarks(Record record, GenericCommand cmd) {
+		
+		Record template = record.copy();
+		template.removeAll(Fields.MESSAGE); 
 
+		List<BookmarkFolder> folders = null;
+		
+		try {
+			folders = suiteMethods.getBookmarksFolders();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if (folders != null) {
+			int i = 1;
+			for (BookmarkFolder f : folders) {
+			    Record outRecord = template.copy();
+			    outRecord.put(Fields.MESSAGE, f);
+		        if (!cmd.getChild().process(outRecord)) {
+		            return false;
+		        }
+		        i++;
+			}
+		} else {
+			return false;
+		}
+		
+		return true;
 	}
 	
 	public Bookmark() {
-	}
 
+	}
+	
 }
