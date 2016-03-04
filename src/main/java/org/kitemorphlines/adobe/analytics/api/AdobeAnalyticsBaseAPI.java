09bb2df596865d0089eeba54fe7be59072ec1b3d
Leonardo Foderaro <startrack79@gmail.com>
2016-03-04 10:05:06 +0100

    added AnalyticsClient and ReportSuiteMethods properties to the base class

--- src/main/java/org/kitemorphlines/adobe/analytics/api/AdobeAnalyticsBaseAPI.java
+++ src/main/java/org/kitemorphlines/adobe/analytics/api/AdobeAnalyticsBaseAPI.java
@@ -1,5 +1,17 @@
 package org.kitemorphlines.adobe.analytics.api;
 
-public class AdobeAnalyticsBaseAPI {
+import com.adobe.analytics.client.AnalyticsClient;
+import com.adobe.analytics.client.methods.ReportSuiteMethods;
+
+public abstract class AdobeAnalyticsBaseAPI {
+	
+	protected AnalyticsClient client;
+
+	protected ReportSuiteMethods suiteMethods;
+	
+	public void setClient(AnalyticsClient client) {
+		this.client = client;
+		suiteMethods = new ReportSuiteMethods(client); //client is created as above
+	}
 
 }
