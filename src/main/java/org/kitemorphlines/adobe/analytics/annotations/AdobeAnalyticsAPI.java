1ef4253178edf3863f973b1370653df30e660733
Leonardo Foderaro <startrack79@gmail.com>
2016-03-05 10:31:52 +0100

    code cleanup! thanks AnnotatedCommands :)

--- src/main/java/org/kitemorphlines/adobe/analytics/annotations/AdobeAnalyticsAPI.java
+++
@@ -1,9 +0,0 @@
-package org.kitemorphlines.adobe.analytics.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface AdobeAnalyticsAPI {
-	String name();
-}
--- src/main/java/org/kitemorphlines/adobe/analytics/annotations/AdobeAnalyticsAPIMethod.java
+++
@@ -1,9 +0,0 @@
-package org.kitemorphlines.adobe.analytics.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface AdobeAnalyticsAPIMethod {
-	public String name();
-}
