50b029a7272d078d9c79ab1948245091ccb254fd
Leonardo Foderaro <startrack79@gmail.com>
2016-03-05 23:10:29 +0100

    Adobe Analytics Bookmarks API - first import

--- src/main/java/org/kitemorphlines/adobe/analytics/api/BookmarksAPI.java
+++ src/main/java/org/kitemorphlines/adobe/analytics/api/BookmarksAPI.java
@@ -2,52 +2,68 @@ package org.kitemorphlines.adobe.analytics.api;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
-import org.kitemorphlines.adobe.analytics.annotations.AdobeAnalyticsAPI;
-import org.kitemorphlines.adobe.analytics.annotations.AdobeAnalyticsAPIMethod;
-import org.kitemorphlines.adobe.analytics.commands.GenericCommand;
+import org.kitesdk.morphline.annotations.KiteCommand;
+import org.kitesdk.morphline.annotations.KiteComponent;
 import org.kitesdk.morphline.api.Record;
 import org.kitesdk.morphline.base.Fields;
+import org.kitesdk.morphline.base.components.KiteCommandContext;
 
+import com.adobe.analytics.client.domain.Bookmark;
 import com.adobe.analytics.client.domain.BookmarkFolder;
 
-@AdobeAnalyticsAPI(name="Bookmark")
-public class Bookmark extends AdobeAnalyticsBaseAPI {
-	
-	@AdobeAnalyticsAPIMethod(name="GetBookmarks")
-	public boolean getBookmarks(Record record, GenericCommand cmd) {
-		
+@KiteComponent
+public class BookmarksAPI {
+
+	@KiteCommand
+	public boolean getBookmarks(Record record, Map<String, Object> params, KiteCommandContext ctx) {
+		AdobeAnalyticsAPI client = (AdobeAnalyticsAPI) record.getFirstValue(AdobeAnalyticsAPI.ANALYTICS_CLIENT);
+
+		if (client == null) {
+			System.out.println("Adobe Analytics Client object not found.");
+			return false;
+		}
+
 		Record template = record.copy();
 		template.removeAll(Fields.MESSAGE); 
 
 		List<BookmarkFolder> folders = null;
-		
+
 		try {
-			folders = suiteMethods.getBookmarksFolders();
+			folders = client.getReportSuiteMethods().getBookmarksFolders();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		if (folders != null) {
-			int i = 1;
 			for (BookmarkFolder f : folders) {
-			    Record outRecord = template.copy();
-			    outRecord.put(Fields.MESSAGE, f);
-		        if (!cmd.getChild().process(outRecord)) {
-		            return false;
-		        }
-		        i++;
+				for (Bookmark bookmark : f.getBookmarks()) {
+					Record outRecord = template.copy();
+					outRecord.put(Fields.MESSAGE, bookmark);
+					if (!ctx.getChild().process(outRecord)) {
+						return false;
+					}
+				}
 			}
 		} else {
 			return false;
 		}
-		
-		return true;
-	}
-	
-	public Bookmark() {
 
+		return true;
+
+	}	
+
+
+	@KiteCommand
+	public boolean processBookmark(Record record, Map<String, Object> params, KiteCommandContext ctx) {
+		Bookmark bookmark = (Bookmark) record.getFirstValue(Fields.MESSAGE);
+
+		System.out.println("found Bookmark: " + bookmark.getName());
+
+		return ctx.getChild().process(record);
 	}
-	
+
+
 }
